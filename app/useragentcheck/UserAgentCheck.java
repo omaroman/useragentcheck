92154e810c9afdda86cc10b5ce3edc7cdd7f5c05
Olivier Refalo <orefalo@tracfone.com>
2011-08-27 08:35:59 -0400

    a few changes to get it working

--- app/useragentcheck/UserAgentCheck.java
+++ app/useragentcheck/UserAgentCheck.java
@@ -5,13 +5,15 @@ import nl.bitwalker.useragentutils.RenderingEngine;
 import nl.bitwalker.useragentutils.UserAgent;
 import nl.bitwalker.useragentutils.Version;
 import play.Logger;
+import play.Play;
 
 public class UserAgentCheck {
 
 	public static boolean check(String agent) {
 
+		Logger.debug(agent);
+
 		try {
-			boolean displayBannerFlag = false;
 
 			UserAgent userAgent = UserAgent.parseUserAgentString(agent);
 			Browser browser = userAgent.getBrowser();
@@ -22,17 +24,64 @@ public class UserAgentCheck {
 
 			int majorVersion = Integer.parseInt(v);
 
+			/** if 0, agent version cannot be parsed, just ignore the test **/
+			boolean displayBannerFlag = false;
+
 			if (majorVersion > 0) {
-				displayBannerFlag = (renderingEngine == "TRIDENT" && majorVersion <= 80);
-				displayBannerFlag |= (renderingEngine == "PRESTO" && majorVersion <= 100);
-				displayBannerFlag |= (renderingEngine == "GECKO" && majorVersion <= 20);
-				displayBannerFlag |= (renderingEngine == "WEBKIT" && majorVersion <= 20);
+				displayBannerFlag = checkAgainst(renderingEngine, "TRIDENT",
+						majorVersion)
+						|| checkAgainst(renderingEngine, "GECKO", majorVersion)
+						|| checkAgainst(renderingEngine, "WEBKIT", majorVersion)
+						|| checkAgainst(renderingEngine, "PRESTO", majorVersion);
 			}
 			return displayBannerFlag;
 		} catch (Exception e) {
-			Logger.warn("UserAgentCheck", agent, e);
+			Logger.warn("UserAgent cannot be parserd %s", e);
 			return false;
 		}
-
 	}
+
+	/**
+	 * returns true if the banner needs to be displayed
+	 */
+	private static boolean checkAgainst(String renderingEngine, String engine,
+			int majorVersion) {
+
+		if (!engine.equals(renderingEngine))
+			return false;
+
+		String capEngine = engine.substring(0, 1).toUpperCase()
+				+ engine.substring(1).toLowerCase();
+		String minVersionStr = Play.configuration
+				.getProperty("useragentcheck.min" + capEngine + "Version");
+		int minVersion;
+
+		if (minVersionStr == null)
+			minVersion = getDefaultMinVersion(engine);
+		else
+			minVersion = Integer.parseInt(minVersionStr);
+
+		return majorVersion < minVersion;
+	}
+
+	public static int getDefaultMinVersion(String engine) {
+
+		if (engine.equals("TRIDENT"))
+			// ie7
+			return 7;
+
+		if (engine.equals("PRESTO"))
+			// opera 9
+			return 9;
+
+		if (engine.equals("GECKO"))
+			// firefox 5
+			return 5;
+		if (engine.equals("WEBKIT"))
+			// webkit 2
+			return 2;
+
+		return 0;
+	}
+
 }
